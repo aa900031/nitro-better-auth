019d823d88fbaf76e71313df8dc2fab3313c2cf9
zhong666 <aa900031@gmail.com>
2025-10-28 17:53:56 +0800

    feat: get better-auth instance from event.context (#34)

--- src/runtime/plugins/auth.ts
+++ src/runtime/plugins/auth.ts
@@ -5,22 +5,35 @@ import { betterAuth as createBetterAuth } from 'better-auth'
 import { defu } from 'defu'
 
 export default defineNitroPlugin((nitroApp) => {
-	let betterAuth: BetterAuthResult | undefined
 	Object.defineProperty(nitroApp, '_betterAuth', {
-		get: () => {
-			if (betterAuth == null) {
-				const opts = loadServerOptions()
-				betterAuth = createBetterAuth(
-					defu(opts, {}),
-				)
-			}
-			return betterAuth
-		},
+		get: () => getter(),
 	})
+	nitroApp.hooks.hook('request', (event) => {
+		Object.defineProperty(event.context, 'betterAuth', {
+			get: () => getter(),
+		})
+	})
+
+	let betterAuth: BetterAuthResult | undefined
+	function getter(): BetterAuthResult | undefined {
+		if (betterAuth == null) {
+			const opts = loadServerOptions()
+			betterAuth = createBetterAuth(
+				defu(opts, {}),
+			)
+		}
+		return betterAuth
+	}
 })
 
 declare module 'nitropack' {
 	interface NitroApp {
 		_betterAuth: BetterAuthResult | undefined
+	}
+}
+
+declare module 'h3' {
+	interface H3EventContext {
+		betterAuth: BetterAuthResult | undefined
 	}
 }
